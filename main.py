2d400d04ae4326d1c996322497de535520625685
Makoto Hashimoto <makoto.hashimoto06@gmail.com>
2023-01-29 14:57:09 +1100

    deployment to heroku

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ from starter.ml.model import inference_preds
 
 # Declare the data object with its components and their type.
 class InferenceData(BaseModel):
-    workclass: str
-    education: str
-    marital_status: str = Field(alias="marital-status")
-    occupation: str
-    relationship: str
-    race: str
-    sex: str
-    native_country: str = Field(alias="native-country")
-    age: int
-    fnlgt: int
-    education_num: int = Field(alias="education-num")
-    capital_gain: int = Field(alias="capital-gain")
-    capital_loss : int = Field(alias="capital-loss")
-    hours_per_week: int = Field(alias="hours-per-week")
+    workclass: str = Field(example="State-gov")
+    education: str = Field(example="Bachelors")
+    marital_status: str = Field(alias="marital-status", example="Never-married")
+    occupation: str = Field(example="Adm-clerical")
+    relationship: str = Field(example="Not-in-family")
+    race: str = Field(example="Adm-clerical")
+    sex: str = Field(example="Male")
+    native_country: str = Field(alias="native-country", example="United-States")
+    age: int = Field(example=39)
+    fnlgt: int = Field(example=77516)
+    education_num: int = Field(alias="education-num", example=13)
+    capital_gain: int = Field(alias="capital-gain", example=2174)
+    capital_loss : int = Field(alias="capital-loss", example=0)
+    hours_per_week: int = Field(alias="hours-per-week", example=40)
 
 app = FastAPI(
     title="Census Bureau data API",
@@ -34,70 +34,9 @@ app = FastAPI(
 
 )
 
-# This allows sending of data (our TaggedItem) via POST to the API.
+# This allows sending of data (our InferenceData) via POST to the API.
 @app.post("/inference")
-async def inference(inferencedata: InferenceData = Body(
-    examples={
-        "normal": {
-            "summary": "A normal example",
-            "description": "A **normal** inrefence data works correctly",
-            "value": {
-                "workclass": "State-gov",
-                "education": "Bachelors",
-                "marital-status": "Never-married",
-                "occupation": "Adm-clerical",
-                "relationship": "Not-in-family",
-                "race": "White",
-                "sex": "Male",
-                "native-country": "United-States",
-                "age": 39,
-                "fnlgt": 77516,
-                "education-num": 13,
-                "capital-gain": 2174,
-                "capital-loss": 0,
-                "hours-per-week": 40
-            },
-        },
-        "invalid": {
-            "summary": "Invalid example",
-            "description": "An **invalid** inrefence data is rejected with an error",
-            "value": {
-                "workclass": 1,
-                "education": "Bachelors",
-                "marital-status": "Never-married",
-                "occupation": "Adm-clerical",
-                "relationship": "Not-in-family",
-                "race": "White",
-                "sex": "Male",
-                "native-countr": "United-States",
-                "age": 39,
-                "fnlgt": 77516,
-                "education-num": 13,
-                "capital-gain": 2174,
-                "capital-loss": 0,
-                "hours-per-week": 40
-            },
-        },
-        "default": {
-            "value": {
-                "workclass": "State-gov",
-                "education": "Bachelors",
-                "marital-status": "Never-married",
-                "occupation": "Adm-clerical",
-                "relationship": "Not-in-family",
-                "race": "White",
-                "sex": "Male",
-                "native-country": "United-States",
-                "age": 39,
-                "fnlgt": 77516,
-                "education-num": 13,
-                "capital-gain": 2174,
-                "capital-loss": 0,
-                "hours-per-week": 40
-            },
-        },
-    },
-)):
+async def inference(inferencedata: InferenceData):
 
     with open("config.yaml", "r") as ymlfile:
         cfg = yaml.safe_load(ymlfile)
